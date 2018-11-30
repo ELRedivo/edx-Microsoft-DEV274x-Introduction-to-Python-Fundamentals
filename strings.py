76961cc509c336848860964e54af4a9becf10c83
ELRedivo <elredivo@gmail.com>
2018-11-29 23:29:57 -0200

    Esqueci

--- strings.py
+++ strings.py
@@ -10,3 +10,35 @@ def invert_string(string):
         counter += -1
     
     return inverted_string
+
+#Slice String
+def slice_string(string,start,stop):
+
+    sliced_string = ''
+    
+    while start < stop:
+
+        sliced_string += string[start]
+        start += 1
+    
+    return sliced_string
+
+#Some things
+iterable = '12345'
+mul_iterables = ''
+
+counter_iterable = 1
+size_iterable = len(iterable)
+
+for item in iterable:
+    
+    mul_iterables += item
+    
+    if counter_iterable == size_iterable:
+        break
+    else:
+        #Separator of strings
+        mul_iterables += '-'
+        counter_iterable += 1
+    
+print(mul_iterables)
